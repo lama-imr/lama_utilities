1336685d8770d66504a80f4526ab04606c276da2
Gael Ecorchard <gael.ecorchard@ciirc.cvut.cz>
2014-09-08 14:23:46 +0000

    lama_jockeys: Add jockey_header.h template
    git-svn-id: https://lynx1.felk.cvut.cz/svn/gael/ros/lama@182 48f9bdac-fdba-e311-b7ba-0011d8a84d3f

---
+++ lama_jockeys/template/jockey_header.h
@@ -0,0 +1,30 @@
+/*
+ * Short description.
+ *
+ * Long description:
+ * - description of general role
+ * - description of used actions with associated result (DONE when, FAIL if...)
+ *
+ * Interaction with the map (created by this jockey):
+ * - [Getter][/][Setter], message type, interface default name
+ * - Getter/Setter: VectorLaserScan, jockey_name + "_laser_descriptor"
+ *
+ * Interaction with the map (created by other jockeys):
+ * - [Getter][/][Setter], message type, interface default name
+ * - Getter: VectorLaserScan, "laser_descriptor"
+ *
+ * Subscribers (other than map-related):
+ * - message type, topic default name, description
+ * - sensor_msgs::LaserScan, "~/base_scan", 360-deg laser-scan.
+ *
+ * Publishers (other than map-related):
+ * - message type, topic default name, description
+ * - nav_msgs::Pose, "~/pose", robot pose
+ *
+ * Services used (other than map-related):
+ * - service type, server default name, description
+ * - polygon_matcher::PolygonSimilarity, "~/similarity_server", used to
+ *    compare all known places with the current place
+ */
+
+

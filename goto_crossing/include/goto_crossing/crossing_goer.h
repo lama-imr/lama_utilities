63289937775ab7a15b3c7d8ed532ef7e0f254c72
Gael Ecorchard <gael.ecorchard@ciirc.cvut.cz>
2014-09-12 08:46:15 +0000

    goto_crossing: improve goToGoal
    git-svn-id: https://lynx1.felk.cvut.cz/svn/gael/ros/lama@193 48f9bdac-fdba-e311-b7ba-0011d8a84d3f

--- goto_crossing/include/goto_crossing/crossing_goer.h
+++ goto_crossing/include/goto_crossing/crossing_goer.h
@@ -4,6 +4,8 @@
 #include <cmath>
 
 #include <ros/ros.h>
+#include <ros/console.h>
+#include <angles/angles.h>
 #include <std_msgs/Bool.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/Twist.h>
@@ -19,23 +21,34 @@ class CrossingGoer
 
     CrossingGoer();
 
-    bool goto_crossing(const lama_msgs::Crossing& crossing, geometry_msgs::Twist& twist) const;
-    void goto_crossing_callback(const lama_msgs::Crossing& crossing) const;
+    bool goto_crossing(const lama_msgs::Crossing& crossing, geometry_msgs::Twist& twist);
+    void goto_crossing_callback(const lama_msgs::Crossing& crossing);
+
+    void resetIntegrals() {sum_v_ = 0; sum_w_ = 0;}
 
   private:
 
-    bool goToGoal(const geometry_msgs::Point& goal, geometry_msgs::Twist& twist) const;
+    bool goToGoal(const geometry_msgs::Point& goal, geometry_msgs::Twist& twist);
 
+    // Parameters shown outside.
+    double kp_v_;  //!> Proportional gain for the linear velocity (s^-1).
+    double kp_w_;  //!> Proportional gain for the angular velocity (s^-1).
+    double ki_v_;  //!> Integral gain for the linear velocity (TODO: unit).
+    double ki_w_;  //!> Integral gain for the angular velocity (TODO: unit).
+    double min_linear_velocity_;  //!> Minimum linear set velocity (m.s^-1)
+    double min_angular_velocity_;  //!> Minimum angular set velocity (m.s^-1)
+    double reach_distance_;  //!> Goal is reached if closer than this (m).
+
+    // Hard-coded parametrs.
+    const static double threshold_w_only_;  //!> If dtheta is greater than this, only turn, do not go forward (rad).
+
+    // Internals.
     ros::NodeHandle nh_;
     ros::Publisher twist_publisher_;
     ros::Publisher goal_reached_publisher_;
-
-    double kp_v_;  //!> Proportional gain for the linear velocity (s^-1).
-    double kp_w_;  //!> Proportional gain for the angular velocity (s^-1).
-    double min_velocity_;  //!> Minimum set velocity (m.s^-1)
-    double reach_distance_;  //!> Goal is reached if closer than this (m).
-
-    const static double threshold_w_only_;  //!> If dtheta is greater than this, only turn, do not go forward (rad).
+    ros::Time last_t_;
+    double sum_v_;  //!> Integral of linear velocity (m).
+    double sum_w_;  //!> Integral of angular velocity (rad).
 };
 
 } // namespace goto_crossing
--- goto_crossing/src/crossing_goer.cpp
+++ goto_crossing/src/crossing_goer.cpp
@@ -1,3 +1,4 @@
+#include <iostream>  // DEBUG
 #include <goto_crossing/crossing_goer.h>
 
 namespace lama {
@@ -6,25 +7,51 @@ namespace goto_crossing {
 const double CrossingGoer::threshold_w_only_ = 1.0;  // (rad), ~60 deg.
 
 CrossingGoer::CrossingGoer() :
-  nh_("~")
+  last_t_(ros::Time::now()),
+  sum_v_(0),
+  sum_w_(0)
 {
-  if (!nh_.getParamCached("kp_v", kp_v_))
-    kp_v_ = 0.05;
-  if (!nh_.getParamCached("kp_w", kp_w_))
+  // Debug log level
+  if(ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug))
+  {
+    ros::console::notifyLoggerLevelsChanged();
+  }
+
+  ros::NodeHandle private_nh;
+  if (!private_nh.getParamCached("kp_v", kp_v_))
+    kp_v_ = 0.1;
+  if (!private_nh.getParamCached("kp_w", kp_w_))
     kp_w_ = 0.2;
-  if (!nh_.getParamCached("min_velocity", min_velocity_))
-    min_velocity_ = 0.020;
-  if (!nh_.getParamCached("reach_distance", reach_distance_))
+  if (!private_nh.getParamCached("ki_v", ki_v_))
+    ki_v_ = 0.00;
+  if (!private_nh.getParamCached("ki_w", ki_w_))
+    ki_w_ = 0.00;
+  if (!private_nh.getParamCached("min_linear_velocity", min_linear_velocity_))
+    min_linear_velocity_ = 0.020;
+  if (!private_nh.getParamCached("min_angular_velocity", min_angular_velocity_))
+    min_angular_velocity_ = 0.1;
+  if (!private_nh.getParamCached("reach_distance", reach_distance_))
     reach_distance_ = 0.050;
 
   twist_publisher_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
   goal_reached_publisher_ = nh_.advertise<std_msgs::Bool>("goal_reached", 1);
-  // TODO: add a timeout mechanism for Crossing reception and set twist to 0.
+  
+  ROS_INFO("CrossingGoer initialized (%s)", ros::this_node::getName().c_str());
+  std::cout << "CrossingGoer initialized (" << ros::this_node::getName() << ")" << std::endl; // DEBUG
 }
 
-bool CrossingGoer::goto_crossing(const lama_msgs::Crossing& crossing, geometry_msgs::Twist& twist) const
+/* Compute the twist needed to reach the goal
+ *
+ * Return true if the goal is reached, false otherwise.
+ *
+ * crossing[in] crossing descriptor relative to the robot
+ * twist[out] twist to apply (all null except linear.x and angular.z).
+ */
+bool CrossingGoer::goto_crossing(const lama_msgs::Crossing& crossing, geometry_msgs::Twist& twist)
 {
   geometry_msgs::Point goal;
+  //!> can only reach the goal if at least 3 exits.
+  bool can_reach = false;
 
   if (crossing.frontiers.size() == 0)
   {
@@ -34,39 +61,51 @@ bool CrossingGoer::goto_crossing(const lama_msgs::Crossing& crossing, geometry_m
   else if (crossing.frontiers.size() == 1)
   {
     // Move to the sole frontier.
-    goal.x = (crossing.frontiers[0].p1.x + crossing.frontiers[0].p2.x) / 2;
-    goal.y = (crossing.frontiers[0].p1.y + crossing.frontiers[0].p2.y) / 2;
+    // DOES NOT WORK VERY WELL.
+    // goal.x = (crossing.frontiers[0].p1.x + crossing.frontiers[0].p2.x) / 2;
+    // goal.y = (crossing.frontiers[0].p1.y + crossing.frontiers[0].p2.y) / 2;
+    goal.x = std::cos(crossing.frontiers[0].angle) * crossing.radius;
+    goal.y = std::sin(crossing.frontiers[0].angle) * crossing.radius;
   }
   else if (crossing.frontiers.size() == 2)
   {
     // Move to the frontier that is the closest to forward.
-    double dtheta0 = std::fmod(crossing.frontiers[0].angle + M_PI, 2 * M_PI) - M_PI;
-    double dtheta1 = std::fmod(crossing.frontiers[1].angle + M_PI, 2 * M_PI) - M_PI;
+    double dtheta0 = angles::normalize_angle(crossing.frontiers[0].angle);
+    double dtheta1 = angles::normalize_angle(crossing.frontiers[1].angle);
     
     if (std::abs(dtheta0) < std::abs(dtheta1))
     {
-      goal.x = (crossing.frontiers[0].p1.x + crossing.frontiers[0].p2.x) / 2;
-      goal.y = (crossing.frontiers[0].p1.y + crossing.frontiers[0].p2.y) / 2;
+      // DOES NOT WORK VERY WELL.
+      // goal.x = (crossing.frontiers[0].p1.x + crossing.frontiers[0].p2.x) / 2;
+      // goal.y = (crossing.frontiers[0].p1.y + crossing.frontiers[0].p2.y) / 2;
+      goal.x = std::cos(dtheta0) * crossing.radius;
+      goal.y = std::sin(dtheta0) * crossing.radius;
     }
     else
     {
-      goal.x = (crossing.frontiers[1].p1.x + crossing.frontiers[1].p2.x) / 2;
-      goal.y = (crossing.frontiers[1].p1.y + crossing.frontiers[1].p2.y) / 2;
+      // DOES NOT WORK VERY WELL.
+      // goal.x = (crossing.frontiers[1].p1.x + crossing.frontiers[1].p2.x) / 2;
+      // goal.y = (crossing.frontiers[1].p1.y + crossing.frontiers[1].p2.y) / 2;
+      goal.x = std::cos(dtheta1) * crossing.radius;
+      goal.y = std::sin(dtheta1) * crossing.radius;
     }
   }
   else
   {
+    can_reach = true;
     // Move to the crossing center.
     goal.x = crossing.center.x;
     goal.y = crossing.center.y;
   }
-  return goToGoal(goal, twist);
+  ROS_DEBUG("goal = (%.3f, %.3f)", goal.x, goal.y);
+  return goToGoal(goal, twist) && can_reach;
 }
 
 /* Callback for the Crossing topic.
  */
-void CrossingGoer::goto_crossing_callback(const lama_msgs::Crossing& crossing) const
+void CrossingGoer::goto_crossing_callback(const lama_msgs::Crossing& crossing)
 {
+  // TODO: add a timeout mechanism for Crossing reception and set twist to 0.
   geometry_msgs::Twist twist;
   bool goal_reached = goto_crossing(crossing, twist);
 
@@ -80,10 +119,13 @@ void CrossingGoer::goto_crossing_callback(const lama_msgs::Crossing& crossing) c
  * 
  * There is no cycle in this function, so it should be called periodically by class instances.
  *
- * goal[in] position of the goal relative to the robot
+ * goal[in] position of the goal relative to the robot.
+ * twist[out] twist to apply (all null except linear.x and angular.z).
  */
-bool CrossingGoer::goToGoal(const geometry_msgs::Point& goal, geometry_msgs::Twist& twist) const
+bool CrossingGoer::goToGoal(const geometry_msgs::Point& goal, geometry_msgs::Twist& twist)
 {
+  ros::Time t = ros::Time::now();
+
   double distance = std::sqrt(goal.x * goal.x + goal.y * goal.y);
 
   if (distance < reach_distance_)
@@ -97,19 +139,39 @@ bool CrossingGoer::goToGoal(const geometry_msgs::Point& goal, geometry_msgs::Twi
   double dtheta = std::atan2(goal.y, goal.x);
   ROS_DEBUG("distance to goal: %f, dtheta to goal: %f", distance, dtheta);
 
-  if (std::fabs(dtheta) > threshold_w_only_)
+  if (std::abs(dtheta) > threshold_w_only_)
   {
     // Do no go forward because the goal is not well in front of the robot.
     distance = 0.0;
   }
 
+  // Compute the integrals.
+  // TODO: Add anti wind-up.
+  double dt = (t - last_t_).toSec();
+  if (dt != 0)
+  {
+    sum_v_ += distance / dt;
+    sum_w_ += dtheta / dt;
+  }
+  last_t_ = t;
+  
   // TODO: add a parameter "allow_backward" so that we don't need to turn 180 deg.
   
-  double vx = kp_v_ * distance; 
-  double wz = kp_w_ * dtheta;
-  if (vx < min_velocity_)
+  double vx = kp_v_ * distance + ki_v_ * sum_v_; 
+  double wz = kp_w_ * dtheta + ki_w_ * sum_w_;
+
+  // Dead-zone management (not need if ki_v_ and ki_w_ non null).
+  if ((vx < min_linear_velocity_) && (distance != 0) && (std::abs(wz) < min_angular_velocity_))
   {
-    vx = min_velocity_;
+    vx = min_linear_velocity_;
+  }
+  if ((wz > 0) && (wz < min_angular_velocity_) && (vx < min_linear_velocity_))
+  {
+    wz = min_angular_velocity_;
+  }
+  else if ((wz < 0) && (wz > -min_angular_velocity_) && (vx < min_linear_velocity_))
+  {
+    wz = -min_angular_velocity_;
   }
 
   twist.linear.x = vx;

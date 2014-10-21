832182f19ae28bfcfcc352fa2e76785584518ec0
Gael Ecorchard <gael.ecorchard@ciirc.cvut.cz>
2014-10-21 10:15:49 +0000

    crossing_detector: saving setup.py before deleting
    git-svn-id: https://lynx1.felk.cvut.cz/svn/gael/ros/lama@255 48f9bdac-fdba-e311-b7ba-0011d8a84d3f

--- crossing_detector/setup.py
+++ crossing_detector/setup.py
@@ -2,10 +2,15 @@
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
+from Cython.Build import cythonize
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['crossing_detector'],
     package_dir={'': 'src'})
+setup_args['ext_modules'] = cythonize(
+    'src/hello.pyx',
+    language="c++"),
+
 
 setup(**setup_args)

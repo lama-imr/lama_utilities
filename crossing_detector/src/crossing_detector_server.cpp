913979fa11d367ff1471bef3033d0e8665440086
Gael Ecorchard <gael.ecorchard@ciirc.cvut.cz>
2014-10-22 05:23:44 +0000

    crossing_detector: add services
    Add DetectCrossing service
    Add LaserDetectCrossing service
    
    
    git-svn-id: https://lynx1.felk.cvut.cz/svn/gael/ros/lama@258 48f9bdac-fdba-e311-b7ba-0011d8a84d3f

---
+++ crossing_detector/src/crossing_detector_server.cpp
@@ -0,0 +1,38 @@
+#include <ros/ros.h>
+#include <ros/console.h> // to change the log level to debug
+
+#include <lama_msgs/Crossing.h>
+#include <lama_msgs/PlaceProfile.h>
+
+#include <crossing_detector/crossing_detector.h>
+#include <crossing_detector/DetectCrossing.h>
+
+lama::crossing_detector::CrossingDetector* detector;
+
+bool detect_crossing_callback(crossing_detector::DetectCrossing::Request& req, crossing_detector::DetectCrossing::Response& res)
+{
+  detector->setFrontierWidth(req.frontier_width);
+  detector->setMaxFrontierAngle(req.max_frontier_angle);
+  detector->setMinRelevance(req.min_relevance);
+  res.crossing = detector->crossingDescriptor(req.profile, true);
+  return true;
+}
+
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "crossing_detector");
+  ros::NodeHandle nh("~");
+  
+  // Change log level.
+  if(ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug))
+  {
+    ros::console::notifyLoggerLevelsChanged();
+  }
+
+  detector = new lama::crossing_detector::CrossingDetector(0);
+
+  ros::ServiceServer service_server = nh.advertiseService("detect_crossing", detect_crossing_callback);
+
+  ros::spin();
+  return 0;
+}
---
+++ crossing_detector/src/laser_crossing_detector_server.cpp
@@ -0,0 +1,38 @@
+#include <ros/ros.h>
+#include <ros/console.h> // to change the log level to debug
+
+#include <lama_msgs/Crossing.h>
+#include <lama_msgs/PlaceProfile.h>
+
+#include <crossing_detector/laser_crossing_detector.h>
+#include <crossing_detector/LaserDetectCrossing.h>
+
+lama::crossing_detector::LaserCrossingDetector* detector;
+
+bool detect_crossing_callback(crossing_detector::LaserDetectCrossing::Request& req, crossing_detector::LaserDetectCrossing::Response& res)
+{
+  detector->setFrontierWidth(req.frontier_width);
+  detector->setMaxFrontierAngle(req.max_frontier_angle);
+  detector->setMinRelevance(req.min_relevance);
+  res.crossing = detector->crossingDescriptor(req.scan, true);
+  return true;
+}
+
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "crossing_detector");
+  ros::NodeHandle nh("~");
+  
+  // Change log level.
+  if(ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug))
+  {
+    ros::console::notifyLoggerLevelsChanged();
+  }
+
+  detector = new lama::crossing_detector::LaserCrossingDetector(0);
+
+  ros::ServiceServer service_server = nh.advertiseService("detect_crossing", detect_crossing_callback);
+
+  ros::spin();
+  return 0;
+}

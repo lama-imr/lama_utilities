8288217faab5eeec9d7298c4c32263f40ab3d826
Gael Ecorchard <gael.ecorchard@ciirc.cvut.cz>
2014-12-08 14:32:10 +0000

    lama_test: get closer to ROS Python style
    git-svn-id: https://lynx1.felk.cvut.cz/svn/gael/ros/lama@313 48f9bdac-fdba-e311-b7ba-0011d8a84d3f

--- lama_test/scripts/test_goto_crossing.py
+++ lama_test/scripts/test_goto_crossing.py
@@ -13,27 +13,30 @@ from visualization_msgs.msg import Marker
 
 from lama_msgs.msg import Crossing
 from lama_msgs.msg import Frontier
+from goto_crossing.srv import ResetIntegrals
+from goto_crossing.srv import ResetIntegralsRequest
 
-g_max_length = 2
+_max_length = 2
 
-g_goal_reached = False
-g_odom = Odometry()
-g_odom.header.frame_id = 'odom'
-g_transform_listener = None
+_goal_reached = False
+_odom = Odometry()
+_odom.header.frame_id = 'odom'
+_transform_listener = None
+_reset_integrals_client = None
 
 
 def callback_goal_reached(msg):
-    global g_goal_reached
-    g_goal_reached = msg.data
+    global _goal_reached
+    _goal_reached = msg.data
 
 
 def callback_odom(msg):
-    global g_odom
-    g_odom = msg
+    global _odom
+    _odom = msg
 
 
 def main():
-    global g_goal_reached
+    global _goal_reached
     crossing = Crossing()
     # We should have at least 3 frontiers for goto_crossing to go
     # to the crossing center.
@@ -54,26 +57,28 @@ def main():
     marker_publisher = rospy.Publisher('~crossing_marker', Marker,
                                        queue_size=1, latch=True)
     while not rospy.is_shutdown():
-        g_goal_reached = False
-        x_center = g_max_length * 2 * (random.random() - 0.5)
-        y_center = g_max_length * 2 * (random.random() - 0.5)
-        target_pose = copy.deepcopy(g_odom.pose.pose)
+        _goal_reached = False
+        srv = ResetIntegralsRequest()
+        _reset_integrals_client.call(srv)
+        x_center = _max_length * 2 * (random.random() - 0.5)
+        y_center = _max_length * 2 * (random.random() - 0.5)
+        target_pose = copy.deepcopy(_odom.pose.pose)
         target_pose.position.x += x_center
         target_pose.position.y += y_center
-        target_pose_stamped = PoseStamped(g_odom.header, target_pose)
+        target_pose_stamped = PoseStamped(_odom.header, target_pose)
         rospy.loginfo('New relative target: ({}, {})'.format(
             x_center, y_center))
         marker.pose.position.x = target_pose.position.x
         marker.pose.position.y = target_pose.position.y
         marker_publisher.publish(marker)
         r = rospy.Rate(10)
-        while not g_goal_reached and not rospy.is_shutdown():
-            target_pose_stamped.header = g_odom.header
-            g_transform_listener.waitForTransform('base_link',
-                                                  g_odom.header.frame_id,
-                                                  g_odom.header.stamp,
-                                                  rospy.Duration(0.1))
-            rel_target_pose_stamped = g_transform_listener.transformPose(
+        while not _goal_reached and not rospy.is_shutdown():
+            target_pose_stamped.header = _odom.header
+            _transform_listener.waitForTransform('base_link',
+                                                 _odom.header.frame_id,
+                                                 _odom.header.stamp,
+                                                 rospy.Duration(0.1))
+            rel_target_pose_stamped = _transform_listener.transformPose(
                 'base_link', target_pose_stamped)
             crossing.center.x = rel_target_pose_stamped.pose.position.x
             crossing.center.y = rel_target_pose_stamped.pose.position.y
@@ -86,10 +91,12 @@ def main():
 
 if __name__ == '__main__':
     rospy.init_node('test_nj_oa', log_level=rospy.INFO)
-    g_transform_listener = tf.TransformListener()
-    g_transform_listener.waitForTransform('odom', 'base_link', rospy.Time(),
-                                          rospy.Duration(5.0))
+    _transform_listener = tf.TransformListener()
+    _transform_listener.waitForTransform('odom', 'base_link', rospy.Time(),
+                                         rospy.Duration(5.0))
 
+    _reset_integrals_client = rospy.ServiceProxy(
+        'goto_crossing/reset_integrals', ResetIntegrals)
     rospy.Subscriber('~goal_reached', Bool, callback_goal_reached)
     rospy.Subscriber('~odom', Odometry, callback_odom)
     main()

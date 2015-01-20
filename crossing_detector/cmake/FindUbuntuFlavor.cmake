37c48893e6f7c2ca065c24ab8d72c244adb33a4a
Gaël Ecorchard <gael.ecorchard@ciirc.cvut.cz>
2015-01-20 11:53:59 +0100

    crossing_detector: solve CGAL cmake on Saucy
    crossing_detector: add cmake msg for debug
    
    crossing_detector: set CGAL libs for saucy
    
    crossing_detector: try another CGAL setup
    
    crossing_detector: remove debug cmake message

---
+++ crossing_detector/cmake/FindUbuntuFlavor.cmake
@@ -0,0 +1,26 @@
+# Detect Ubuntu Flavor
+# The following variable will be set
+# $UBUNTU_DIST (e.g. UBUNTU_DIST=14.04)
+
+if (NOT WINDOWS)	
+  if (EXISTS "/etc/os-release")
+    execute_process(
+      COMMAND grep VERSION_ID=
+      COMMAND sed -e "s/VERSION_ID=\"\\(.*\\)\"/\\1/"
+      INPUT_FILE "/etc/os-release"
+      RESULT_VARIABLE RES
+      OUTPUT_VARIABLE UBUNTU_DIST
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_QUIET
+      )
+
+    if (RES EQUAL 0)
+      message (STATUS "Building for Ubuntu ${UBUNTU_DIST}")
+    else()
+      message (STATUS "Ubuntu not detected")
+    endif()
+
+  endif()
+
+endif()	
+

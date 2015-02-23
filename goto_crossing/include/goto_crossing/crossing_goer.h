cee8172c72054e450e5ab5d8d6126e0465fb6094
Gaël Ecorchard <gael.ecorchard@ciirc.cvut.cz>
2015-02-23 10:44:16 +0100

    goto_crossing: update doxygen documentation

--- goto_crossing/include/goto_crossing/crossing_goer.h
+++ goto_crossing/include/goto_crossing/crossing_goer.h
@@ -33,20 +33,20 @@ class CrossingGoer
     bool callback_resetIntegrals(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res);
 
     // Parameters shown outside.
-    double kp_v_;  //!> Proportional gain for the linear velocity (s^-1).
-    double kp_w_;  //!> Proportional gain for the angular velocity (s^-1).
-    double ki_v_;  //!> Integral gain for the linear velocity (s^-2).
-    double ki_w_;  //!> Integral gain for the angular velocity (s^-2).
-    double min_linear_velocity_;  //!> Minimum linear set velocity (m.s^-1), used if the integral gain is 0.
-    double min_angular_velocity_;  //!> Minimum angular set velocity (m.s^-1), used if the integral gain is 0.
-    double reach_distance_;  //!> Goal is reached if closer than this (m).
-    double dtheta_force_left_;  //!> If the goal is behind the robot, force the robot to turn left if the goal angle
-                                //!> is within [pi - dtheta_force_left_, pi], even if the angle difference would tell
-                                //!> that we should turn right. This is to avoid instability in the case that the angle
-                                //!> would oscillate around pi. (rad)
-    double threshold_w_only_;  //!> If dtheta is greater than this, only turn, do not go forward (rad).
-    double max_sum_v_;  //!> Anti wind-up for sum_v_ (m.s).
-    double max_sum_w_;  //!> Anti wind-up for sum_w_ (rad.s).
+    double kp_v_;  //!< Proportional gain for the linear velocity (s^-1).
+    double kp_w_;  //!< Proportional gain for the angular velocity (s^-1).
+    double ki_v_;  //!< Integral gain for the linear velocity (s^-2).
+    double ki_w_;  //!< Integral gain for the angular velocity (s^-2).
+    double min_linear_velocity_;  //!< Minimum linear set velocity (m.s^-1), used if the integral gain is 0.
+    double min_angular_velocity_;  //!< Minimum angular set velocity (m.s^-1), used if the integral gain is 0.
+    double reach_distance_;  //!< Goal is reached if closer than this (m).
+    double dtheta_force_left_;  //!< If the goal is behind the robot, force the robot to turn left if the goal angle
+                                //!< is within [pi - dtheta_force_left_, pi], even if the angle difference would tell
+                                //!< that we should turn right. This is to avoid instability in the case that the angle
+                                //!< would oscillate around pi. (rad)
+    double threshold_w_only_;  //!< If dtheta is greater than this, only turn, do not go forward (rad).
+    double max_sum_v_;  //!< Anti wind-up for sum_v_ (m.s).
+    double max_sum_w_;  //!< Anti wind-up for sum_w_ (rad.s).
 
     // Internals.
     ros::NodeHandle nh_;

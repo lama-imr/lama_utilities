78e0a13f95604a780dcf8653cec8ea4d9fe6e889
Karel Košnar <kosnark@ciirc.cvut.cz>
2015-12-10 16:12:15 +0100

    commit changes before change branch

--- nlj_dummy/src/nlj_dummy_node.cpp
+++ nlj_dummy/src/nlj_dummy_node.cpp
@@ -7,6 +7,7 @@
 
 #include <nlj_dummy/nj_dummy.h>
 #include <nlj_dummy/lj_dummy.h>
+//#include <lama_msgs/JockeyInfo.h>
 
 #include <lama_interfaces/AddInterface.h>
 
@@ -32,9 +33,18 @@ int main(int argc, char **argv)
   // Run the jockeys.
   LJDummy loc_jockey("localizing_jockey", srv.request.interface_name, srv.response.set_service_name);
   NJDummy nav_jockey("navigating_jockey", srv.response.get_service_name);
+ /* lama_msgs::JockeyInfo ji;
+  ji.name = "localizing_jockey";
+  ji.type = lama_msgs::JockeyInfo::LOCALIZING_JOCKEY;
+  ji.interface_name="dummy_descriptor";
+//  loc_jockey.registerToExecutor(ji);
+  ji.name = "navigating_jockey";
+  ji.type = lama_msgs::JockeyInfo::NAVIGATING_JOCKEY;
+  ji.interface_name="dummy_descriptor";
+//  nav_jockey.registerToExecutor(ji);
 
   ROS_DEBUG("dummy_jockey started");
-  ros::spin();
+  ros::spin();*/
   return 0;
 }
 
